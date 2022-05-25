70df894259a9471d13e9cd84a08ca2dd1d5a71f8
NO8ODY <steffen@emayl.me>
2022-05-25 18:41:10 +0200

    use latest swarm version

--- installer.sh
+++ installer.sh
@@ -40,7 +40,7 @@ if [ ! -f "/var/lib/swarm/swarm" ]; then
         if [ ! -z "$keyboardInputUsername" ] && [ ! -z "$keyboardInputPassword" ]; then
             latestSwarmVersion=$(curl --max-time 5 -s https://api.github.com/repos/TangleBay/swarm-releases/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')
             latestSwarmVersion=$(echo $latestSwarmVersion | tr -d 'v')
-            checkSwarmUpdateAuth=$(curl -s -o /dev/null -w "%{http_code}" https://$keyboardInputUsername:$keyboardInputPassword@service.tanglebay.com/download/swarm/v$latestSwarmVersion/checksum.txt)
+            checkSwarmUpdateAuth=$(curl -s -o /dev/null -w "%{http_code}" https://$keyboardInputUsername:$keyboardInputPassword@service.tanglebay.com/download/swarm/latest/checksum.txt)
             if [ "$checkSwarmUpdateAuth" = "200" ] && [ ! -z "$latestSwarmVersion" ]; then
                 clear
                 echo ""
@@ -68,18 +68,18 @@ if [ ! -f "/var/lib/swarm/swarm" ]; then
 
                     echo -e $TEXT_RED_B && echo "-> Downloading SWARM..." && echo -e $TEXT_RESET
                     if [ ! -d "$swarmTmp" ]; then
-                        sudo mkdir -p $swarmTmp/v$latestSwarmVersion > /dev/null 2>&1
+                        sudo mkdir -p $swarmTmp/installer > /dev/null 2>&1
                     fi
-                    sudo wget -q -O $swarmTmp/v$latestSwarmVersion/swarm-v$latestSwarmVersion.tar.gz https://$keyboardInputUsername:$keyboardInputPassword@service.tanglebay.com/download/swarm/v$latestSwarmVersion/swarm-v$latestSwarmVersion.tar.gz
+                    sudo wget -q -O $swarmTmp/installer/swarm-latest.tar.gz https://$keyboardInputUsername:$keyboardInputPassword@service.tanglebay.com/download/swarm/latest/swarm-latest.tar.gz
                     echo ""
                     echo -e $TEXT_RED_B && echo "-> Verify checksum of SWARM..." && echo -e $TEXT_RESET
                     echo ""
-                    swarmChkSum=$(curl -s https://$keyboardInputUsername:$keyboardInputPassword@service.tanglebay.com/download/swarm/v$latestSwarmVersion/checksum.txt)
-                    swarmUpdateChkSum=$(shasum -a 512 $swarmTmp/v$latestSwarmVersion/swarm-v$latestSwarmVersion.tar.gz | awk '{ print $1 }')
+                    swarmChkSum=$(curl -s https://$keyboardInputUsername:$keyboardInputPassword@service.tanglebay.com/download/swarm/latest/checksum.txt)
+                    swarmUpdateChkSum=$(shasum -a 512 $swarmTmp/installer/swarm-latest.tar.gz | awk '{ print $1 }')
                     if [ "$swarmChkSum" = "$swarmUpdateChkSum" ]; then
-                        ( cd $swarmTmp/v$latestSwarmVersion ; sudo tar -xzf $swarmTmp/v$latestSwarmVersion/swarm-v$latestSwarmVersion.tar.gz ) > /dev/null 2>&1
-                        if [ -f "$swarmTmp/v$latestSwarmVersion/swarm/swarm" ]; then
-                            sudo cp -rf $swarmTmp/v$latestSwarmVersion/swarm /var/lib > /dev/null 2>&1
+                        ( cd $swarmTmp/installer ; sudo tar -xzf $swarmTmp/installer/swarm-latest.tar.gz ) > /dev/null 2>&1
+                        if [ -f "$swarmTmp/installer/swarm/swarm" ]; then
+                            sudo cp -rf $swarmTmp/installer/swarm /var/lib > /dev/null 2>&1
 
                             if [ -f "/var/lib/swarm/swarm" ]; then
                                 echo -e $TEXT_RED_B && echo "-> Loading env..." && echo -e $TEXT_RESET
